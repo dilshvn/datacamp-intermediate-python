9726ad1fe0e4d39b75b0d256fbe34f2b2e2691f3
Dilshan Deshapriya Perera <dilshvn@gmail.com>
2022-11-09 10:09:46 +0530

    Add files via upload

---
+++ codes/Loop over NumPy array.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for val in np_height:
+    print(f'{val} inches')
+
+# For loop over np_baseball
+for val in np.nditer(np_baseball):
+    print(val)
